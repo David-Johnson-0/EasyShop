ae42cf4f9d15b6dae25fa1e1fadeca66dbea2586
davidjohnson <soccerdavejohnson@gmail.com>
2026-03-12 14:01:32 -0500

    updated paths

--- easyshopapp/urls.py
+++ easyshopapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
     path('item/', views.item, name='item'),
     path('checkout/', views.checkout, name='checkout'),
