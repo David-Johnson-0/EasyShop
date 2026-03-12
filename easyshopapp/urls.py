0bcb96b6832c376458ded36db3c8e18e9372d3e3
davidjohnson <soccerdavejohnson@gmail.com>
2026-03-12 14:05:34 -0500

    updated theme for website

--- easyshopapp/urls.py
+++ easyshopapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
     path('item/', views.item, name='item'),
     path('checkout/', views.checkout, name='checkout'),
